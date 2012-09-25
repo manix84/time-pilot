3badf1e58466e111ac18a22512fb5fd79c3e7a20
Rob Taylor <manix84@gmail.com>
2012-09-25 22:11:45 +0100

    Creating a generic draw method and connecting to it.

--- Launch.js
+++ Launch.js
@@ -12,21 +12,19 @@ define([
     };
 
     TimePilot.prototype = {
-        init: function () {
-            this._createCanvas();
-            this._renderShip();
+        _characterStore: {
+            player: {
+                src: "../sprites/ship.png",
+                frameWidth: 32,
+                frameHeight: 32,
+                frameCount: 16
+            }
         },
 
-        _tickCallback: function () {
-            var SPEED = 1;
-            this._player.positionY += (Math.cos(this._player.heading * (Math.PI / 180)) * SPEED).toFixed(15);
-            this._player.positionX += (Math.sin(this._player.heading * (Math.PI / 180)) * SPEED).tiFixed(15);
-        },
         _liveDataStore: {
             level: 1,
 
             player: {
-                spriteRef: null,
                 heading: 90,
                 positionX: 0,
                 positionY: 0
@@ -44,6 +42,17 @@ define([
             bullets: []
         },
 
+        init: function () {
+            this._createCanvas();
+            this._renderShip();
+        },
+
+        _tickCallback: function () {
+            var SPEED = 1;
+            this._player.positionY += (Math.cos(this._player.heading * (Math.PI / 180)) * SPEED).toFixed(15);
+            this._player.positionX += (Math.sin(this._player.heading * (Math.PI / 180)) * SPEED).tiFixed(15);
+        },
+
         _createCanvas: function () {
             this._canvas = document.createElement('canvas');
             this._container.appendChild(this._canvas);
@@ -55,36 +64,33 @@ define([
             this._canvasContext = this._canvas.getContext('2d');
         },
 
+        _drawCharacter: function (characterData) {
+            this._canvasContext.drawImage(
+                characterData,
+                characterData.frameWidth,
+                0,
+                characterData.frameWidth,
+                characterData.frameHeight,
+                ((this._container.clientWidth / 2) - (characterData.frameWidth / 2)),
+                ((this._container.clientHeight / 2) - (characterData.frameHeight / 2)),
+                characterData.frameWidth,
+                characterData.frameHeight
+            );
+        },
+
         _drawEnemies: function () {
             var i = 0,
                 l = this._enemies.length;
             for (; i < l; i++) {
                 // DRAW ENEMY
+                this._drawCharacter();
             }
         },
 
-        _setupPlayer: function () {
-            this._liveDataStore.player.spriteRef.src = "../sprites/ship.png";
-            this._liveDataStore.player.spriteRef.frameWidth = 32;
-            this._liveDataStore.player.spriteRef.frameHeight = 32;
-            this._liveDataStore.player.spriteRef.frameCount = 16;
-        },
-
         _drawPlayer: function () {
-
-            this._canvasContext.drawImage(
-                this._liveDataStore.player.spriteRef,
-                this._liveDataStore.player.spriteRef.frameWidth,
-                0,
-                this._liveDataStore.player.spriteRef.frameWidth,
-                this._liveDataStore.player.spriteRef.frameHeight,
-                ((this._container.clientWidth / 2) - (this._liveDataStore.player.spriteRef.frameWidth / 2)),
-                ((this._container.clientHeight / 2) - (this._liveDataStore.player.spriteRef.frameHeight / 2)),
-                this._liveDataStore.player.spriteRef.frameWidth,
-                this._liveDataStore.player.spriteRef.frameHeight
-            );
-
+            this._draw(this._liveDataStore.player);
         },
+
         _renderMenu: function () {},
         _renderSky: function () {}
     };
