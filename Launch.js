9e2c86385d66035031edb06f595b519029dddc2a
Rob Taylor <manix84@gmail.com>
2012-09-29 12:43:32 +0100

    Adding canvas error message.

--- Launch.js
+++ Launch.js
@@ -85,7 +85,12 @@ define(function () {
 
             this._canvas.setAttribute('height', this._gameData.container.height);
             this._canvas.setAttribute('width', this._gameData.container.width);
-            // this._canvas.innerHTML = this._options.supportError;
+            this._canvas.innerHTML =
+                "<div style='padding:3px;background:#CCC'>" +
+                    "<img src='../images/supportError.png' style='position:relative;margin:7px' />" +
+                    "<p>Looks like your browser doesn't support the HTML5 canvas.</p>" +
+                    "<p>Please consider updating to a more modern browser such as <a href=''>Google Chrome</a> or <a href=''>Mozilla FireFox</a>.</p>" +
+                "</div>";
 
             this._canvasContext = this._canvas.getContext('2d');
         },
