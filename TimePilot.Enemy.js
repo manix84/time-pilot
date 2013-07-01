8f3c858f805d86af6ad93a25bac66e386662f591
Manix84 <manix84@gmail.com>
2013-07-02 00:46:17 +0100

    Adding new enemy factory and enemy class. Removing old in-built enemy code.

--- TimePilot.Enemy.js
+++ TimePilot.Enemy.js
@@ -1,4 +1,6 @@
-define("TimePilot.Enemy", function () {
+define("TimePilot.Enemy", [
+    "lib/helpers"
+], function (helpers) {
 
     /**
      * Level specific data about the player.
@@ -7,43 +9,44 @@ define("TimePilot.Enemy", function () {
      */
     var LEVEL_DATA = {
         1: {
+            src: "./sprites/enemy_level1.png",
             velocity: 3,
             turn: 5,
             height: 32,
             width: 32,
-            hitRadius: 8
+            firingChance: 0.2,
+            hitRadius: 8,
+            canRotate: true
         }
     };
 
     /**
-     * [ description]
+     * Creates an enemy to add to the page.
      * @constructor
-     * @param   {[type]} canvas [description]
-     * @param   {[type]} ticker [description]
-     * @param   {[type]} player [description]
-     * @returns {[type]}
+     * @param   {Object}            gameRules   - Object containing basic game rules.
+     * @param   {Canvas Instance}   canvas      - Canvas Instance.
+     * @param   {Ticker Instance}   ticker      - Ticker Instance.
+     * @param   {Player Instance}   player      - Player Instance.
+     * @param   {Number}            posX        - Spawning location on the X axis.
+     * @param   {Number}            posY        - Spawning location on the Y axis.
+     * @param   {Number}            heading     - Start heading (usually towards the player).
+     * @returns {Enemy Instance}
      */
-    var Enemy = function (canvas, ticker, player) {
+
+    var Enemy = function (gameRules, canvas, ticker, player, posX, posY, heading) {
+        this._gameRules = gameRules;
         this._canvas = canvas;
         this._player = player;
         this._ticker = ticker;
+
+        this._data = {};
+        this._data.posX = posX;
+        this._data.posY = posY;
+        this._data.heading = heading;
+        this._data.isInArena = true;
     };
 
     Enemy.prototype = {
-
-        /**
-         * Stored data about the player.
-         * @type {Object}
-         */
-        _data: {
-            isFiring: false,
-            heading: 90,
-            posX: 0,
-            posY: 0,
-            height: 32,
-            width: 32,
-            hitRadius: 8
-        },
 
         /**
          * Get data for the player.
@@ -90,7 +93,7 @@ define("TimePilot.Enemy", function () {
         /**
          * Get current data for this level
          * @method
-         * @returns {[type]}
+         * @returns {object}
          */
         getLevelData: function () {
             return LEVEL_DATA[this._level];
@@ -101,69 +104,25 @@ define("TimePilot.Enemy", function () {
          * @method
          */
         reposition: function () {
-            var i = 0, j = 0,
-                l = this._data.level.current,
-                sprite = new Image(),
-                h, s, a;
-
-            sprite.src = this._options.baseUrl + "sprites/enemy_level" + l + ".png";
-            sprite.frameWidth = this._data.width;
-            sprite.frameHeight = this._data.height;
-            // Shorten enemy heading and game level.
+            var enemy = this._data,
+                heading = this._data.heading,
+                levelData = this.getLevelData(),
+                player = this._player.getData(),
+                canvas = this._canvas,
+                turnTo;
 
             // Per-Enemy Data
-            sprite.frameX = Math.floor(h / 22.5);
-            sprite.frameY = (Math.floor(this._ticker.getTicks() / 10) % 2);
 
-            this._data.posX += parseFloat((Math.sin(h * (Math.PI / 180)) * s).toFixed(5));
-            this._data.posY -= parseFloat((Math.cos(h * (Math.PI / 180)) * s).toFixed(5));
+            enemy.posX += helpers.float(Math.sin(heading * (Math.PI / 180)) * levelData.velocity);
+            enemy.posY -= helpers.float(Math.cos(heading * (Math.PI / 180)) * levelData.velocity);
 
-
-            a = this._findAngle(this._data, {
-                posX: this._player.getData().posX + ((this._data.container.width / 2) - (this._data.width / 2)),
-                posY: this._player.getData().posY + ((this._data.container.height / 2) - (this._data.height / 2))
+            turnTo = helpers.findHeading(enemy, {
+                posX: player.posX + ((canvas.width / 2) - (levelData.width / 2)),
+                posY: player.posY + ((canvas.height / 2) - (levelData.height / 2))
             });
-            a = (Math.floor(a / 22.5) * 22.5);
+            turnTo = (Math.floor(turnTo / 22.5) * 22.5);
 
-            this._data.heading = this._rotateTo(a, this._data.heading, 22.5);
-
-            sprite.posX = (this._data.posX - this._player.getData().posX - (this._data.width / 2));
-            sprite.posY = (this._data.posY - this._player.getData().posY - (this._data.height / 2));
-
-            this._canvas.renderSprite(sprite);
-
-            if (this._detectCollision({
-                // Enemy Position
-                posX: (this._data.posX - this._player.getData().posX),
-                posY: (this._data.posY - this._player.getData().posY),
-                radius: this._data.hitRadius
-            }, {
-                // Bullet Position
-                posX: this._player.getData().posX + (this._player.getData().width / 2),
-                posY: this._player.getData().posX + (this._player.getData().height / 2),
-                radius: this._player.getData().hitRadius
-            })) {
-                window.console.warn("GAME OVER ", this._data.score);
-            }
-
-            for (j = 0; j < this._data.bullets.length; j++) {
-                if (this._detectCollision({
-                    // Enemy Position
-                    posX: (this._data.posX - this._player.getData().posX),
-                    posY: (this._data.posY - this._player.getData().posY),
-                    radius: this._data.hitRadius
-                }, {
-                    // Bullet Position
-                    posX: this._data.bullets[j].posX + (this._data.level[l].bullet.size / 2),
-                    posY: this._data.bullets[j].posY + (this._data.level[l].bullet.size / 2),
-                    radius: this._data.level[l].bullet.hitRadius
-                })) {
-                    this._addExplosion(this._data.posX, this._data.posY);
-                    this._data.enemies.splice(i, 1);
-                    this._data.bullets.splice(j, 1);
-                    this._data.score += 100;
-                }
-            }
+            enemy.heading = helpers.rotateTo(turnTo, enemy.heading, 22.5);
         },
 
         /**
@@ -171,19 +130,17 @@ define("TimePilot.Enemy", function () {
          * @method
          */
         render: function () {
-            var sprite = {
-                src: "./sprites/enemy_level" + this._level + ".png",
-                frameWidth: 32,
-                frameHeight: 32,
+            var levelData = this.getLevelData();
+            this._canvas.renderSprite(levelData.src, {
+                frameWidth: levelData.width,
+                frameHeight: levelData.height,
                 frameX: Math.floor(this._data.heading / 22.5),
-                frameY: 0,
-                posX: ((this._canvas.getCanvas().width / 2) - (sprite.frameWidth / 2)),
-                posY: ((this._canvas.getCanvas().height / 2) - (sprite.frameWidth / 2))
-            };
-
-            this._canvas.renderSprite(
-                sprite
-            );
+                frameY: (Math.floor(this._ticker.getTicks() / 10) % 2),
+                posX: (this._data.posX - this._player.getData().posX - (levelData.width / 2)),
+                posY: (this._data.posY - this._player.getData().posY - (levelData.height / 2))
+            });
         }
     };
+
+    return Enemy;
 });
--- TimePilot.EnemyFactory.js
+++ TimePilot.EnemyFactory.js
@@ -1,6 +1,7 @@
 define("TimePilot.EnemyFactory", [
+    "lib/helpers",
     "TimePilot.Enemy"
-], function (Enemy) {
+], function (helpers, Enemy) {
     /**
      * Construct an enemy factory for managing creation, movement, rendering and removal of enemies.
      * @constructor
@@ -9,11 +10,13 @@ define("TimePilot.EnemyFactory", [
      * @param   {Player Instance} player - Player Instance
      * @returns {Enemy Factory Instance}
      */
-    var EnemyFactory = function (canvas, ticker, player) {
-        this._enemies = [];
+    var EnemyFactory = function (gameRules, canvas, ticker, player) {
+        this._gameRules = gameRules;
         this._canvas = canvas;
         this._player = player;
         this._ticker = ticker;
+
+        this._enemies = [];
     };
 
     EnemyFactory.prototype = {
@@ -21,9 +24,9 @@ define("TimePilot.EnemyFactory", [
          * Create an enemy instance and keep a record of it in the factory.
          * @method
          */
-        create: function () {
+        create: function (posX, posY, heading) {
             this._enemies.push(
-                new Enemy(this._canvas, this._ticker, this._player)
+                new Enemy(this._gameRules, this._canvas, this._ticker, this._player, posX, posY, heading)
             );
         },
 
@@ -32,18 +35,31 @@ define("TimePilot.EnemyFactory", [
          * @method
          */
         reposition: function () {
-            var i = 0,
-                len = this._enemies.length;
+            var player = this._player.getData(),
+                levelData, i, isInArena;
 
-            for (; i < len; ++i) {
-                this._enemies[i].reposition();
+
+            for (i in this._enemies) {
+                if (this._enemies.hasOwnProperty(i)) {
+                    this._enemies[i].reposition();
+                    levelData = this._enemies[i].getLevelData();
+                    isInArena = helpers.detectAreaExit({
+                            posX: player.posX + ((this._canvas.width / 2) - (levelData.width / 2)),
+                            posY: player.posY + ((this._canvas.height / 2) - (levelData.height / 2))
+                        },
+                        this._enemies[i].getData(),
+                        500
+                    );
+                    if (!isInArena) {
+                        this.despawn(i);
+                    }
+                }
             }
         },
 
         /**
          * Render all enemies on the canvas.
          * @method
-         * @returns {[type]}
          */
         render: function () {
             var i = 0,
@@ -52,6 +68,11 @@ define("TimePilot.EnemyFactory", [
             for (; i < len; ++i) {
                 this._enemies[i].render();
             }
+        },
+
+        despawn: function (entityId) {
+            window.console.log("Despawning", this._enemies[entityId]);
+            this._enemies.splice(entityId, 1);
         }
     };
 
