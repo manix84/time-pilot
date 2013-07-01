c11f2804119df57d95cd882d2b309d7de0f91365
Manix84 <manix84@gmail.com>
2013-07-01 15:35:51 +0100

    Adding comments. My bad for not adding them first.

--- TimePilot.EnemyFactory.js
+++ TimePilot.EnemyFactory.js
@@ -1,6 +1,14 @@
 define("TimePilot.EnemyFactory", [
     "TimePilot.Enemy"
 ], function (Enemy) {
+    /**
+     * Construct an enemy factory for managing creation, movement, rendering and removal of enemies.
+     * @constructor
+     * @param   {Canvas Instance} canvas - Canvas Instance
+     * @param   {Ticker Instance} ticker - Ticker Instance
+     * @param   {Player Instance} player - Player Instance
+     * @returns {Enemy Factory Instance}
+     */
     var EnemyFactory = function (canvas, ticker, player) {
         this._enemies = [];
         this._canvas = canvas;
@@ -9,12 +17,20 @@ define("TimePilot.EnemyFactory", [
     };
 
     EnemyFactory.prototype = {
+        /**
+         * Create an enemy instance and keep a record of it in the factory.
+         * @method
+         */
         create: function () {
             this._enemies.push(
                 new Enemy(this._canvas, this._ticker, this._player)
             );
         },
 
+        /**
+         * Run all reposition logic.
+         * @method
+         */
         reposition: function () {
             var i = 0,
                 len = this._enemies.length;
@@ -24,6 +40,11 @@ define("TimePilot.EnemyFactory", [
             }
         },
 
+        /**
+         * Render all enemies on the canvas.
+         * @method
+         * @returns {[type]}
+         */
         render: function () {
             var i = 0,
                 len = this._enemies.length;
