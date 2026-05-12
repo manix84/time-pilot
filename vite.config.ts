2026f7e28d6ee6cafc1e770c78e2680f56098994
Rob Taylor <373278+manix84@users.noreply.github.com>
2026-05-12 11:19:24 +0100

    feat: Update import for defineConfig to use vitest/config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   base: process.env.VITE_BASE_PATH ?? "/",
